1a0594f41f6bba3c2ae2a7d4494aa9097899af58
gaurav yadav <>
2018-11-26 00:52:13 +0530

    Added a custom user profile model and manager and our database migration.

--- src/todo_project/todo_project/settings.py
+++ src/todo_project/todo_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'todo_app',
+    'todo_app.apps.TodoAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'todo_app.UserProfile'
